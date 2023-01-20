5c459bbfc0e95c5357b1bd91479875a1f5e1d26c
Travis Keating <travis-kea@live.com>
2023-01-20 13:17:24 -0500

    progress

--- Front End Programming/car reservation project/reservation.js
+++ Front End Programming/car reservation project/reservation.js
@@ -4,7 +4,7 @@ pickup_date.focus()
 
 $(document).ready( () => {
 
-	const reservation_form = document.getElementById(reservation_form);
+	const reservation_form = document.getElementById('reservation_form');
 	const pickup_date = document.getElementById('pickup_date');
 	const days = document.getElementById('day_count');
 	const vehicle_type = document.getElementById('vehicle_type');
@@ -16,56 +16,71 @@ $(document).ready( () => {
 	const phone = document.getElementById('phone_number');
 	const form = document.getElementById('form')
 	
-	
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault()
-		
-		console.log('here test');
-		checkInputs();
+		;
+		validateForm();
 	});
 
 	
 		const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
 		
-		function checkInputs() {
+		function validateForm() {
 			//remove white space from the values
-			const pickup_date_value = pickup_date.value.trim();
-			const days_value = days.value.trim();
-			const name_value = name.value.trim();
-			const email_value = email.value.trim();
-			const phone_value = phone.value.trim();
+			let pickup_date_value = pickup_date.value.trim();
+			let days_value = days.value.trim();
+			let name_value = name.value.trim();
+			let email_value = email.value.trim();
+			let phone_value = phone.value.trim();
 
+			//converts the day value into a number
+			let days_value_parsed = parseInt(days_value, 10);
+			console.log(days_value_parsed);
+
+			//determine if the input is valid
 			if(pickup_date_value === ''){
-				setError(pickup_date, "Please select a pickup date");
-			} else {
-				setSuccess(pickup_date);
+				document.getElementById('pickup_error').innerHTML = 'Pick-up date is required.'
+				
+			}else{
+				document.getElementById('pickup_error').innerHTML = ''
+
 			}
 
-			if(days_value === ''){
-				setError(days, "Please select a number of days");
+			if(days_value_parsed === ''){
+				document.getElementById('day_count_error').innerHTML = 'Number of days is required.'
+				console.log('here');
+			} 
+			//Determine if day count is a number
+			else if(days_value_parsed == 'NaN') {
+				document.getElementById('day_count_error').innerHTML = "Must be a number"
+				
 			} else {
-				setSuccess(days);
+				document.getElementById('day_count_error').innerHTML = ''
 			}
 
 			if(name_value === ''){
-				setError(name, "Please enter your name");
+				document.getElementById('name_error').innerHTML = 'Name is required.'
+
 			} else {
-				setSuccess(name);
+				document.getElementById('name_error').innerHTML = ''
+
 			}
 
 			if(email_value === ''){
-				setError(email, "Email address is required");
-			} else if (validEmail(email_value)){
-				setError(email, 'Please provide a valid email address');
+				document.getElementById('email_error').innerHTML = 'Email is required.'
+				
 			} else {
-				setSuccess(email);
+				document.getElementById('email_error').innerHTML = ''
+				
 			}
 		
 			if(phone_value === ''){
-				setError(phone, "Please select a number of days");
+				document.getElementById('phone_error').innerHTML = 'Phone number is required.'
+				
 			} else {
-				setSuccess(phone);
+				document.getElementById('phone_error').innerHTML = ''
+				
 			}
 		};
 	
